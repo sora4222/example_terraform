1137b4364e7dd4d829bc832fc3749f79e70a60c9
Jesse Ross <sora4222@gmail.com>
2018-12-22 21:52:39 +1100

    Adds a Setup_by tag

--- main.tf
+++ main.tf
@@ -5,5 +5,8 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d26157"
   instance_type = "t2.micro"
+  tags{
+      Name = "terraformed-ec2-instance"
+      Setup_by = "terraform"
+  }
 }
-
