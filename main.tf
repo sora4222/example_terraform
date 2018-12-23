92fe6119382bd77f533201662bd057b055df6434
Jesse Ross <sora4222@gmail.com>
2018-12-23 17:11:13 +1100

    Adds the web server to the system

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "web_server" {
+  ami = ""
+  instance_type = "t2.micro"
+  vpc_security_group_ids= ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+  tags {
+      Name="simple-web-server"
+      Provided_by="Terraform"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name="terraform-web-server-security"
+
+  ingress{
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
