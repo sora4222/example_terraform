620e1e74f9b6f3a773cf1710289b2d773e5597c0
Jesse Ross <sora4222@gmail.com>
2018-12-25 21:51:02 +1100

    Adds a load balancer

--- main.tf
+++ main.tf
@@ -49,9 +49,54 @@ resource "aws_autoscaling_group" "cluster" {
   min_size=2
   max_size=5
 
+  load_balancers = ["${aws_elb.load_balancer.name}"]
+  health_check_type = "ELB"
+
   tag{
     key = "Name"
     value="Terraform-example"
     propagate_at_launch=true
   }
+}
+
+resource "aws_elb" "load_balancer" {
+  name="terraform-load-balancer-example"
+  availability_zones=["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol="http"
+  }
+
+  health_check{
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elastic-load-balancer"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "Elastic_load_balancer" {
+  value = "${aws_elb.load_balancer.dns_name}"
 }
